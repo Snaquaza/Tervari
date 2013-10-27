d7e79f17cbe4bd148b449155115390aed0dab0a6
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-10-27 06:29:16 -0700

    Update pokedex.js

--- mods/tervari/pokedex.js
+++ mods/tervari/pokedex.js
@@ -1,4 +1,198 @@
 exports.BattlePokedex = {
+        butterfree: {
+                inherit: true,
+                baseStats: {hp:60,atk:45,def:50,spa:80,spd:80,spe:70}
+        },
+        beedrill: {
+                inherit: true,
+                baseStats: {hp:65,atk:80,def:40,spa:45,spd:80,spe:75}
+        },
+        pikachu: {
+                inherit: true,
+                baseStats: {hp:35,atk:55,def:30,spa:50,spd:40,spe:90}
+        },
+        raichu: {
+                inherit: true,
+                baseStats: {hp:60,atk:90,def:55,spa:90,spd:80,spe:100}
+        },
+        clefairy: {
+                inherit: true,
+                types:["Normal"]
+        },
+        clefable: {
+                inherit: true,
+                types:["Normal"],
+                baseStats: {hp:95,atk:70,def:73,spa:85,spd:90,spe:60}
+        },
+        jigglypuff: {
+                inherit: true,
+                types:["Normal"],
+                abilities: {0:"Cute Charm",H:"Friend Guard"}
+        },
+        wigglytuff: {
+                inherit: true,
+                types:["Normal"],
+                baseStats: {hp:140,atk:70,def:45,spa:75,spd:50,spe:45},
+                abilities: {0:"Cute Charm",H:"Frisk"}
+        },
+        alakazam: {
+                inherit: true,
+                baseStats: {hp:55,atk:50,def:45,spa:135,spd:85,spe:120}
+        },
+        golem: {
+                inherit: true,
+                baseStats: {hp:80,atk:110,def:130,spa:55,spd:65,spe:45}
+        },
+        mrmime: {
+                inherit: true,
+                types:["Psychic"]
+        },
+        igglybuff: {
+                inherit: true,
+                types:["Normal"],
+                abilities: {0:"Cute Charm",H:"Friend Guard"}
+        },
+        togepi: {
+                inherit: true,
+                types:["Normal"]
+        },
+        togetic: {
+                inherit: true,
+                types:["Normal","Flying"]
+        },
+        cleffa: {
+                inherit: true,
+                types:["Normal"]
+        },
+        marill: {
+                inherit: true,
+                types:["Water"]
+        },
+        azumarill: {
+                inherit: true,
+                types:["Water"],
+                baseStats: {hp:100,atk:50,def:80,spa:50,spd:80,spe:50}
+        },
+        snubbull: {
+                inherit: true,
+                types:["Normal"]
+        },
+        granbull: {
+                inherit: true,
+                types:["Normal"]
+        },
+        ralts: {
+                inherit: true,
+                types:["Psychic"]
+        },
+        kirlia: {
+                inherit: true,
+                types:["Psychic"]
+        },
+        gardevoir: {
+                inherit: true,
+                types:["Psychic"]
+        },
+        azurill: {
+                inherit: true,
+                types:["Normal"]
+        },
+        mawile: {
+                inherit: true,
+                types:["Steel"]
+        },
+        plusle: {
+                inherit: true,
+                abilities: {0:"Plus"}
+        },
+        minun: {
+                inherit: true,
+                abilities: {0:"Minus"}
+        },
+        kecleon: {
+                inherit: true,
+                abilities: {0:"Color Change"}
+        },
+        milotic: {
+                inherit: true,
+                abilities: {0:"Marvel Scale",H:"Cute Charm"}
+        },
+        duskull: {
+                inherit: true,
+                abilities: {0:"Levitate"}
+        },
+        dusclops: {
+                inherit: true,
+                abilities: {0:"Pressure"}
+        },
+        starly: {
+                inherit: true,
+                abilities: {0:"Keen Eye"}
+        },
+        staraptor: {
+                inherit: true,
+                baseStats: {hp:85,atk:120,def:70,spa:50,spd:50,spe:100}
+        },
+        mimejr: {
+                inherit: true,
+                types:["Psychic"]
+        },
+        togekiss: {
+                inherit: true,
+                types:["Normal","Flying"]
+        },
+        dusknoir: {
+                inherit: true,
+                abilities: {0:"Pressure"}
+        },
+        venipede: {
+                inherit: true,
+                abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+        },
+        whirlipede: {
+                inherit: true,
+                abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+        },
+        scolipede: {
+                inherit: true,
+                baseStats: {hp:60,atk:90,def:89,spa:55,spd:69,spe:112},
+                abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+        },
+        whimsicott: {
+                inherit: true,
+                types:["Grass"]
+        },
+        gothita: {
+                inherit: true,
+                abilities: {0:"Frisk",H:"Shadow Tag"}
+        },
+        gothorita: {
+                inherit: true,
+                abilities: {0:"Frisk",H:"Shadow Tag"}
+        },
+        gothitelle: {
+                inherit: true,
+                abilities: {0:"Frisk",H:"Shadow Tag"}
+        },
+        ferroseed: {
+                inherit: true,
+                abilities: {0:"Iron Barbs"}
+        },
+        ferrothorn: {
+                inherit: true,
+                abilities: {0:"Iron Barbs"}
+        },
+        litwick: {
+                inherit: true,
+                abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+        },
+        lampent: {
+                inherit: true,
+                abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+        },
+        chandelure: {
+                inherit: true,
+                abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
 viterb:{num:850,species:"Viterb",types:["Grass"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:53,atk:63,def:41,spa:55,spd:46,spe:51},abilities:{0:"Overgrow",DW:"Leaf Guard"},heightm:2,weightkg:100,color:"Green",evos:["hierbado"],evoLevel:32,eggGroups:["Monster","Plant"]},
 hierbado:{num:851,species:"Hierbado",types:["Grass"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:62,atk:83,def:51,spa:78,spd:56,spe:83},abilities:{0:"Overgrow",DW:"Leaf Guard"},heightm:2,weightkg:100,color:"Green",prevo:"viterb",evoLevel:32,eggGroups:["Monster","Plant"]},
 rapagram:{num:852,species:"Rapagram",types:["Grass","Rock"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:80,atk:112,def:75,spa:81,spd:75,spe:105},abilities:{0:"Overgrow",DW:"Leaf Guard"},heightm:2,weightkg:100,color:"Green",prevo:"hierbado",evoLevel:36,eggGroups:["Monster","Plant"]},
