a887fdf71bfce864e71d74fa5fbbfea4e9989276
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-10-27 08:21:31 -0700

    brb

--- mods/kalos2/pokedex.js
+++ mods/kalos2/pokedex.js
@@ -1,1 +1,23 @@
-//start
+exports.BattlePokedex = {
+        latias: {
+                inherit: true,
+                types: ["Dragon","Fairy"]
+                baseStats: {hp:90,atk:65,def:100,spa:105,spd:140,spe:110}
+       },
+       flygon: {
+                inherit: true,
+                types: ["Bug","Ground"]
+                abilites: {0:"Levtitate",1:"Compoundeyes",H:"Tinted Lens"}
+                baseStats: {hp:80,atk:110,def:100,spa:80,spd:95,spe:105}
+       },
+       mew: {
+                inherit: true,
+                types: ["Psychic","Fairy"]
+                abilites: {0:"Regenerator",H:"Synchronise"}
+       },
+       mew: {
+                inherit: true,
+                types: ["Psychic","Fairy"]
+                abilites: {0:"Regenerator",H:"Synchronise"}
+       },
+};
