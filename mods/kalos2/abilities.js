8dc28a86b443851c3e7118f477f225f9afec6a1d
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-11-09 11:16:28 -0800

    add protean nerf

--- mods/kalos2/abilities.js
+++ mods/kalos2/abilities.js
@@ -20,5 +20,22 @@ exports.BattleAbilities = {
                 rating: 4.5,
                 num: -6,
                 gen: 6
-        },
+        }
+        "protean": {
+                desc: "Changes user's type to match the user's current move before it attacks.",
+                shortDesc: "Changes user's type to match its move.",
+                onBeforeMove: function(pokemon, target, move) {
+                        if (!move) return;
+                        var moveType = (move.id === 'hiddenpower' ? pokemon.hpType : move.type);
+                        if (pokemon.types.join() !== moveType) {
+                                this.add('-start', pokemon, moveType, 'typechange', '[from] Protean');
+                                pokemon.types = [moveType];
+                        }
+                },
+                id: "protean",
+                name: "Protean",
+                rating: 1.5,
+                num: -6,
+                gen: 6
+        }, 
 };
