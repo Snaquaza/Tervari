ec3c0e2b16aa030d63fc9c28e87b24cffa6b2e6c
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-11-09 10:51:09 -0800

    FINISH POKEDEX OMG THAT TOOK SO LONG

--- mods/kalos2/pokedex.js
+++ mods/kalos2/pokedex.js
@@ -87,12 +87,109 @@ exports.BattlePokedex = {
        talonflame: {
                 inherit: true,
                 abilites: {0:"Flame Body",1:"Flash Fire",H:"Gale Wings"}
-                baseStats: {hp:80,atk:100,def:100,spa:80,spd:95,spe:126}
+                baseStats: {hp:78,atk:100,def:71,spa:80,spd:69,spe:126}
+       },
+       vivillon: {
+                inherit: true,
+                types: ["Bug","Fairy"]
+                baseStats: {hp:80,atk:52,def:85,spa:105,spd:85,spe:105}
+       },
+       gogoat: {
+                inherit: true,
+                abilites: {0:"Sap Sipper",H:"Regenerator"}
+                baseStats: {hp:123,atk:105,def:62,spa:80,spd:81,spe:68}
+       },
+       pyroar: {
+                inherit: true,
+                abilites: {0:"Rivalry",1:"Unnerve",H:"Solar Power"}
+                baseStats: {hp:86,atk:68,def:68,spa:110,spd:66,spe:106}
+       },
+       pangoro: {
+                inherit: true,
+                abilites: {0:"Iron Fist",1:"Moxie",H:"Scrappy"}
+                baseStats: {hp:110,atk:130,def:78,spa:69,spd:71,spe:75}
+       },
+       meowstic: {
+                inherit: true,
+                types: ["Psychic","Dark"]
+                abilites: {0:"Magic Bounce",1:"Infiltrator",H:"Prankster"}
+                baseStats: {hp:100,atk:70,def:95,spa:70,spd:100,spe:105}
+       },
+       meowsticf: {
+                inherit: true,
+                types: ["Psychic","Normal"]
+                abilites: {0:"Magic Guard",1:"Infiltrator",H:"Competetive"}
+                baseStats: {hp:80,atk:105,def:80,spa:110,spd:60,spe:110}
+       },
+       doublade: {
+                inherit: true,
+                abilites: {0:"Frisk"}
+                baseStats: {hp:80,atk:115,def:130,spa:45,spd:65,spe:30}
+       },
+       aegislash: {
+                inherit: true,
+                types: ["Ghost"]
+                baseStats: {hp:85,atk:55,def:140,spa:55,spd:90,spe:55}
+       },
+       aegislashblade: {
+                inherit: true,
+                types: ["Ghost"]
+                baseStats: {hp:65,atk:125,def:55,spa:125,spd:55,spe:100}
+       },
+       furfrou: {
+                inherit: true,
+                baseStats: {hp:90,atk:80,def:70,spa:80,spd:100,spe:110}
+       },
+       malamar: {
+                inherit: true,
+                abilites: {0:"Contrary",1:"Infiltrator",H:"Prankster"}
+                baseStats: {hp:80,atk:105,def:90,spa:100,spd:95,spe:80}
+       },
+       barbaracle: {
+                inherit: true,
+                abilites: {0:"Tough Claws",1:"Sniper",H:"Sand Rush"}
+                baseStats: {hp:80,atk:110,def:86,spa:54,spd:115,spe:72}
+       },
+       goodra: {
+                inherit: true,
+                abilites: {0:"Regenerator",1:"Dry Skin",H:"Gooey"}
+       },
+       clawitzer: {
+                inherit: true,
+                types: ["Bug","Water"]
+                abilites: {0:"Mega Launcher",1:"Compoundeyes"}
+                baseStats: {hp:71,atk:135,def:88,spa:85,spd:89,spe:80}
+       },
+       dragalge: {
+                inherit: true,
+                baseStats: {hp:90,atk:75,def:90,spa:100,spd:123,spe:60}
+       },
+       chespin: {
+                inherit: true,
+                types: ["Grass","Ground"]
+                baseStats: {hp:70,atk:70,def:80,spa:33,spd:55,spe:60}
+       },
+       fennekin: {
+                inherit: true,
+                types: ["Fire","Psychic"]
+                baseStats: {hp:60,atk:20,def:60,spa:85,spd:70,spe:85}
+       },
+       froakie: {
+                inherit: true,
+                types: ["Water","Dark"]
+                baseStats: {hp:42,atk:61,def:40,spa:70,spd:50,spe:90}
+       },
+       fletchling: {
+                inherit: true,
+                types: ["Fire","Flying"]
+                baseStats: {hp:55,atk:70,def:50,spa:65,spd:45,spe:80}
        },
        latias:{num:380,species:"Latias",types:["Dragon","Fairy"],gender:"F",baseStats: {hp:90,atk:65,def:100,spa:105,spd:140,spe:110},abilities:{0:"Levitate"},heightm:1.4,weightkg:40,color:"Red",eggGroups:["No Eggs"],otherFormes:["latiasmega"]},
        latiasmega:{num:380,species:"Latias-Mega",baseSpecies:"Latias",forme:"Mega",formeLetter:"M",types:["Dragon","Fairy"],gender:"N",baseStats:{hp:120,atk:65,def:100,spa:130,spd:165,spe:135},abilities:{0:"Magic Bounce"},heightm:3,weightkg:215,color:"Red",eggGroups:["No Eggs"]},
        latios:{num:381,species:"Latios",types:["Dragon","Psychic"],gender:"M",baseStats:{hp:90,atk:100,def:65,spa:140,spd:105,spe:110},abilities:{0:"Levitate"},heightm:2,weightkg:60,color:"Blue",eggGroups:["No Eggs"],otherFormes:["latiosmega"]},
        latiosmega:{num:381,species:"Latios-Mega",baseSpecies:"Latios",forme:"Mega",formeLetter:"M",types:["Dragon","Psychic"],gender:"N",baseStats:{hp:70,atk:150,def:68,spa:190,spd:72,spe:165},abilities:{0:"Magic Guard"},heightm:3,weightkg:215,color:"Blue",eggGroups:["No Eggs"]},
+       arceus:{num:493,species:"Arceus",baseForme:"Normal",types:["Normal"],gender:"N",baseStats:{hp:120,atk:120,def:120,spa:120,spd:120,spe:120},abilities:{0:"Multitype"},heightm:3.2,weightkg:320,color:"Gray",eggGroups:["No Eggs"],otherFormes:["arceusbug","arceusdark","arceusdragon","arceuselectric","arceusfighting","arceusfire","arceusflying","arceusghost","arceusgrass","arceusground","arceusice","arceuspoison","arceuspsychic","arceusrock","arceussteel","arceuswater","arceusgod"]},
+       arceusgod:{num:493,species:"Arceus-God",baseSpecies:"Arceus",forme:"God",formeLetter:"G",types:["Normal","Fairy"],gender:"N",baseStats:{hp:140,atk:140,def:140,spa:140,spd:140,spe:140},abilities:{0:"Protean"},heightm:5.8,weightkg:203,color:"Grey",eggGroups:["No Eggs"]}
        xerneas:{num:716,species:"Xerneas",types:["Fairy"],gender:"N",baseStats:{hp:126,atk:131,def:95,spa:131,spd:98,spe:99},abilities:{0:"Fairy Aura"},heightm:3,weightkg:215,color:"",eggGroups:["No Eggs"],otherFormes:["xerneasmega"]},
        yveltal:{num:717,species:"Yveltal",types:["Dark","Flying"],gender:"N",baseStats:{hp:126,atk:131,def:95,spa:131,spd:98,spe:99},abilities:{0:"Dark Aura"},heightm:5.8,weightkg:203,color:"",eggGroups:["No Eggs"],otherFormes:["yveltalmega"]},
        xerneasmega:{num:716,species:"Xerneas-Mega",baseSpecies:"Xerneas",forme:"Mega",formeLetter:"M",types:["Fairy","Grass"],gender:"N",baseStats:{hp:165,atk:60,def:135,spa:115,spd:165,spe:105},abilities:{0:"Natural Cure"},heightm:3,weightkg:215,color:"",eggGroups:["No Eggs"]},
