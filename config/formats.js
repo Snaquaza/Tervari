d91c3722bd220435f3d3261877081e508502112c
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-11-23 10:47:18 -0800

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -1070,14 +1070,6 @@ exports.Formats = [
 		banlist: ['Charizardite-X']
 	},
        {
-		name: "Kalos 2.0 BH",
-		section: "Kalos 2.0",
-
-                mod: 'kalos2', 
-		ruleset: ['OHKO Clause'],
-                banlist: ['Wonder Guard', 'Pure Power', 'Huge Power', 'Shadow Tag', 'Arena Trap']
-       },
-       {
 		name: "Kalos 2.0 Doubles",
 		section: "Kalos 2.0",
 
